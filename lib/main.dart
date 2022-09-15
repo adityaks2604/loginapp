0416a016c5259b086ea84613710c44da2b4c6f41
Adityaks <krsinghaditya98@gmail.com>
2022-09-16 02:51:23 +0530

    adding homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'homepage.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -24,7 +24,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const 
+      HomePage(),
     );
   }
 }
